65b8377e2c6eeb7b60742ff183e4cd0c12111324
mughieams <mughieams24@gmail.com>
2020-03-16 05:46:53 +0700

    feat(db): add crud fuction

--- lib/rentalcar/database.rb
+++ lib/rentalcar/database.rb
@@ -30,6 +30,28 @@ module RentalCar
             date DATE
          )"
       end
+  
+      def read(table)
+        query "SELECT * FROM #{table}"
+      end
+  
+      def insert(table, _data = {})
+        _data.delete_if { |_key, value| value.to_s.strip == '' }
+        query "INSERT INTO #{table} (#{_data.keys.map(&:to_s).join(',')})
+          VALUES (#{_data.values.map { |value| "'#{value}'" }.join(',')})"
+      end
+  
+      def update(table, reg_number, _data = {})
+        _data.delete_if { |_key, value| value.to_s.strip == '' }
+        query "UPDATE #{table} SET
+            #{_data.map { |key, value| "#{key}='#{value}'" }.join(',')}
+         WHERE reg_number like '#{reg_number}'"
+      end
+  
+      def find(table, param, value)
+        query "SELECT * FROM #{table}
+        WHERE #{param} like '#{value}'"
+      end
     end
   end
   
